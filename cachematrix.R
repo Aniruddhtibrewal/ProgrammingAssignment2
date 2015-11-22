b7e5298978adc389457f72088ade5c2fa4c535c5
Aniruddhtibrewal <aniruddh1406@gmail.com>
2015-11-23 00:05:06 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+makecachematrix <- function (X = matrix()) ## function to set & get value of X  and set & get value of inverse##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+{
+    matrixinverse <- NULL   ## Initialize inverse to Null##
+    
+    set <- function (y = matrix())  ## set X to user defined value in parent enviornment ##
+      {  
+          X <<- y
+          matirxinverse<<- NULL
+      }
+  
+    get <- function () X
+    
+    setinverse <- function(inverse)  ## set inverse to calculated inverse value in parent enviornment##
+      {
+          matrixinverse <<-inverse
+          return(matrixinverse)
+      }
+      
+    getinverse <- function() matirxinverse
+    
+    list (set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
+## check if the inverse exist and return the value else calculated and return inverse##
 
-## Write a short comment describing this function
+cacheinverse <- function(X, ...)
+  {
+      calculatedmatrixinverse <- X$getinverse() ##Assign the current inverse to a variable##
+  
+      if(!is.null(calculatedmatrixinverse)) ##check if the inverse already exists and return if exists##
+        {
+            message("getting inverse from cache")
+            return(calculatedmatrixinverse)
+        }
+  
+      matrix <- X$get() ## else get the value of the matrix##
+      calculatedmatrixinverse <- solve(matrix) ## calculate inverse##
+      message("value of inverse is ")
+      X$setinverse(calculatedmatrixinverse) ##Set and return inverse##
+  }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
